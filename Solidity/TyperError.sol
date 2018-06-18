e861e4f34492b91b38e23e5024e848e3b44da32c
Daniel <szego.daniel@gmail.com>
2018-06-18 01:45:17 -0700

    added two more examples

---
+++ Solidity/TyperError.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.18;
+
+
+// typer errors
+contract Type {
+    
+    function TypeConversion() view returns (uint){
+        uint i = 500;
+        uint8 j = uint8(i);
+        return j;
+    }
+}
+
+// forcing ether to a contract
+contract NonPayableContract {
+    function getContractBalance() view public returns(uint) {
+        return this.balance;
+    }
+    
+    function() payable {
+        revert();
+    }
+}
+
+contract ForceEtherToContract {
+    address contractAddress;
+    
+    constructor(address aToForce) {
+        contractAddress = aToForce;
+    }
+    
+    function () payable {
+        
+    }
+    
+    function kill() {
+        selfdestruct(contractAddress);
+    }
+    
+    function getContractBalance() view public returns(uint) {
+        return this.balance;
+    }
+}
+
+
+//private variable
+contract PrivateVariable{
+    string private Password = "IXperience123" ;
+} 
+
+//web3.eth.getStorageAt(contractAddress, 0)
+
+//reentrancy
+
+
+
